091d60b838dd5d659b213612a5fe93d425303feb
ajchili <kirinpatel@gmail.com>
2018-10-31 12:38:21 -0400

    Pushed v0.1.0 to App and Play Stores

--- react-native/android/app/src/main/java/com/kirinpatel/ablyHistory/AblyHistory.java
+++ react-native/android/app/src/main/java/com/kirinpatel/ablyHistory/AblyHistory.java
@@ -4,10 +4,7 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableMap;
 import com.google.gson.Gson;
-
-import java.util.ArrayList;
 
 import io.ably.lib.realtime.AblyRealtime;
 import io.ably.lib.realtime.Channel;
