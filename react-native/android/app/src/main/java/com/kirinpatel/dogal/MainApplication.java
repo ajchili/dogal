3dc417e58d6d94bbe73ff749f35db84575dfe8cf
Kirin Patel <kirinpatel@gmail.com>
2018-11-03 13:51:36 -0400

    Added react-native-color-matrix-image-filters

--- react-native/android/app/src/main/java/com/kirinpatel/dogal/MainApplication.java
+++ react-native/android/app/src/main/java/com/kirinpatel/dogal/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.kirinpatel.ablyHistory.AblyHistoryPackage;
+import iyegoroff.RNColorMatrixImageFilters.RNColorMatrixImageFiltersPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.asList(
                     new MainReactPackage(),
-                    new AblyHistoryPackage()
+                    new AblyHistoryPackage(),
+                    new RNColorMatrixImageFiltersPackage()
             );
         }
 
