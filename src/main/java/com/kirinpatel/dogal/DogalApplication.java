758c74729ef4b8f418ef7a9a7bd350e3ce9d7cf6
ajchili <kirinpatel@gmail.com>
2019-11-25 14:03:34 -0500

    Added Spring Boot backend

---
+++ src/main/java/com/kirinpatel/dogal/DogalApplication.java
@@ -0,0 +1,13 @@
+package com.kirinpatel.dogal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirinpatel/dogal/DogalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirinpatel.dogal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
